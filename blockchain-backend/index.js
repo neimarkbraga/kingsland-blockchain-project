d4b1159e721a58108ee32c9c83e8f484bef2cf5c
Makoy <neimarkbraga@gmail.com>
2019-04-29 10:36:37 +0800

    initialized blockchain-backend routes

--- blockchain-backend/index.js
+++ blockchain-backend/index.js
@@ -15,6 +15,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // init routes
 app.use('/', require('./app/routes/index'));
+app.use('/debug', require('./app/routes/debug'));
+app.use('/blocks', require('./app/routes/blocks'));
+app.use('/transactions', require('./app/routes/transactions'));
+app.use('/balances', require('./app/routes/balances'));
+app.use('/address', require('./app/routes/address'));
+app.use('/peers', require('./app/routes/peers'));
+app.use('/mining', require('./app/routes/mining'));
 
 
 // start server
