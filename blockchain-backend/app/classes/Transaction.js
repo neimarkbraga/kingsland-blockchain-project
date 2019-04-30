191e9b98818aa5cdb4dd7e826d8e24a024bff880
Makoy <neimarkbraga@gmail.com>
2019-04-30 11:33:51 +0800

    updated Transaction Class

--- blockchain-backend/app/classes/Transaction.js
+++ blockchain-backend/app/classes/Transaction.js
@@ -1,15 +1,31 @@
+const utils = require('../libraries/utils');
+
 module.exports = class Transaction {
-    constructor() {
-        this.from                = 0;
-        this.to                  = 0;
-        this.value               = 0;
-        this.fee                 = 0;
-        this.dateCreated         = 0;
-        this.data                = 0;
-        this.senderPubkey        = 0;
-        this.transactionDataHash = 0;
-        this.senderSignature     = []; // array
-        this.minedInBlockIndex   = 0;
-        this.transferSuccessful  = false;
+
+    constructor(from, to, value, fee, dateCreated, data, senderPubKey, transactionDataHash, senderSignature, minedInBlockIndex, transferSuccessful) {
+        this.from                   = from;
+        this.to                     = to;
+        this.value                  = value;
+        this.fee                    = fee;
+        this.dateCreated            = dateCreated;
+        this.data                   = data;
+        this.senderPubkey           = this.getDataHash();
+        this.transactionDataHash    = transactionDataHash;
+        this.senderSignature        = senderSignature;
+        this.minedInBlockIndex      = minedInBlockIndex;
+        this.transferSuccessful     = transferSuccessful;
+    }
+
+    // get data hash
+    getDataHash() {
+        return utils.sha256(JSON.stringify({
+            from:           this.from,
+            to:             this.to,
+            value:          this.value,
+            fee:            this.fee,
+            dateCreated:    this.dateCreated,
+            data:           this.data,
+            senderPubkey:   this.senderPubkey
+        }));
     }
 };
