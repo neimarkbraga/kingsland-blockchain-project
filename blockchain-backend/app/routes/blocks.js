fa43f14d7ed881484c9565cf95866c3dc10ebb9a
Makoy <neimarkbraga@gmail.com>
2019-04-30 11:52:23 +0800

    updated blocks toutes

--- blockchain-backend/app/routes/blocks.js
+++ blockchain-backend/app/routes/blocks.js
@@ -1,6 +1,21 @@
 const express = require('express');
+
 let app = express.Router();
 
 // endpoints here!
+app.get('/', (req, res) => {
+    res.json(APP_NODE.chain.blocks);
+});
+
+app.get('/:index', (req, res) => {
+    let index = parseInt(req.params.index || 0) || 0;
+    let block = APP_NODE.chain.blocks[index];
+
+    if(block) res.json(block);
+    else {
+        res.status(404);
+        res.json({errorMsg: 'Invalid block index'});
+    }
+});
 
 module.exports = app;
