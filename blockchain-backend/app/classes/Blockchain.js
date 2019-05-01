dbaa33e7161da8fd74ed4964f0a4336e0b489506
John Christian Los Bañes <jglosbanes@up.edu.ph>
2019-05-01 11:14:36 +0800

    Partial Implementation of route /get-mining-job/:minerAddress

--- blockchain-backend/app/classes/Blockchain.js
+++ blockchain-backend/app/classes/Blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./Block");
+const Miner = require("./Miner");
 const Transaction = require('./Transaction');
 const config = require('../../config');
 
@@ -8,7 +9,7 @@ module.exports = class BlockChain {
         this.blocks = [BlockChain.createGenesisBlock()];
         this.pendingTransactions = [];
         this.currentDifficulty = 0;
-        this.miningJobs = [] //map <blockDataHash -> block>
+        this.miningJobs = new Map; //map <blockDataHash -> block>
     }
 
     static createGenesisBlock() {
@@ -37,6 +38,33 @@ module.exports = class BlockChain {
             config.genesis_date,    // dateCreated
             undefined               // blockHash
         );
+    }
+
+    sendMiningJob(minerAddress) {
+        if (!this.pendingTransactions) {
+            return;
+        }
+
+        let candidateBlock = new Block(
+                                        this.blocks.length + 1,   // index
+                                        this.pendingTransactions, // transactions
+                                        this.currentDifficulty,   // difficulty
+                                        "prevBlockHash",          // prevBlockHash
+                                        0,                        // minedBy
+                                        "blockDataHash",          // blockDataHash
+                                        0,                        // nonce
+                                        new Date(),               // dateCreated
+                                        undefined                 // blockHash
+                                      );
+
+        let miner = new Miner(
+                                minerAddress,
+                                this.currentDifficulty,
+                                candidateBlock.getDataHash(),
+                             );
+
+        // ADD TO MINING JOBS
+        // this.miningJobs.set(this.candidateBlock.getDataHash(), this.blocks.length + 1);
     }
 
     getBlockByIndex(index) {
--- blockchain-backend/app/routes/mining.js
+++ blockchain-backend/app/routes/mining.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const globals = require('../../globals');
+
 let app = express.Router();
 
 // endpoints here!
+
+app.get('/get-mining-job/:minerAddress', (req, res) => {
+    let minerAddress = req.params.minerAddress || 0;
+
+    globals.node.getChain().sendMiningJob(minerAddress);
+
+    res.json(minerAddress);
+});
 
 module.exports = app;
