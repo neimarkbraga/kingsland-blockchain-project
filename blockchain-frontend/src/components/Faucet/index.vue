a7baa6d21bd0b9ea1e0c99e510436f2b55e45d12
John Christian Los Bañes <jglosbanes@up.edu.ph>
2019-05-07 16:22:30 +0800

    Explorer - Faucet (Update UI)

--- blockchain-frontend/src/components/Faucet/index.vue
+++ blockchain-frontend/src/components/Faucet/index.vue
@@ -1,50 +1,59 @@
 <template>
     <div class="container">
         <!-- spacer -->
-        <div style="height: 50px;"></div>
+        <div style="height: 20vh;"></div>
 
         <!-- captcha tester -->
-        <div class="card">
-            <div class="card-content">
-                <div class="card-title">Google ReCaptcha Tester</div>
-                <div style="padding-top: 10px">
-                    <div class="input-field">
-                        <label>Response</label>
-                        <textarea class="materialize-textarea"
-                                  v-model="captchaResponse"
-                                  placeholder="" readonly></textarea>
-                    </div>
-                    <div class="center-align">
-                        <div>
-                            <app-captcha @response="captchaResponse = $event"
-                                         ref="Captcha"></app-captcha>
+        <div class="row">
+            <div class="col s6 offset-s3">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-title">
+                            <b>Microcoin Faucet:</b>
                         </div>
-                        <div v-if="captchaResponse">
-                            <button type="button"
-                                    @click.prevent="resetCaptcha"
-                                    class="btn">
-                                Reset Captcha
-                            </button>
+                        <div style="padding-top: 70px">
+                            <div class="center-align">
+                                <div>
+                                    <app-captcha @response="captchaResponse = $event"
+                                                ref="Captcha"></app-captcha>
+                                </div>
+                                <div v-if="captchaResponse">
+                                    <button type="button"
+                                            @click.prevent="resetCaptcha"
+                                            class="btn">
+                                        Reset Captcha
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row center-align">
+                            <div class="row">
+                                <div class="input-field col s10 offset-s1">
+                                    <input id="recipient" type="text" class="validate" name=recipient v-model="recipient">
+                                    <label for="recipient">Enter Recipient Address</label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <button class="btn blue center-align" v-on:click.prevent="sendBalance">
+                                    Send <i>5000000</i> microCoins
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div id="loadWalletTab" class="row center-align">
-            <div class="row">
-                <div class="input-field col offset-s4 s4">
-                    <input id="recipient" type="text" class="validate" name=recipient v-model="recipient">
-                    <label for="recipient">Enter Recipient</label>
-                </div>
-            </div>
-            <div class="row">
-                <button class="btn blue center-align" v-on:click.prevent="sendBalance">Send Balance</button>
-            </div>
-        </div>
-
     </div>
 </template>
+
+<style scoped>
+    .card-title {
+        padding-top: 10px;
+    }
+</style>
+
+
 <script>
     import AppCaptcha from '../Plugins/captcha';
     const axios = require('axios');
