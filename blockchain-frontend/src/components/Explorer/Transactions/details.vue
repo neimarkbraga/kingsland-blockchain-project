bb3790356e77e3dee807e811aadee98002b25443
Makoy <neimarkbraga@gmail.com>
2019-05-11 00:03:05 +0800

    added error handling in transaction details page

--- blockchain-frontend/src/components/Explorer/Transactions/details.vue
+++ blockchain-frontend/src/components/Explorer/Transactions/details.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col s12">
                 <router-link to="/explorer" class="breadcrumb dark">Explorer</router-link>
-                <router-link :to="{name: 'confirmed'}" class="breadcrumb dark" v-if="transaction.minedInBlockIndex !== undefined">
+                <router-link :to="{name: 'confirmed'}" class="breadcrumb dark" v-if="transaction && transaction.minedInBlockIndex !== undefined">
                     Confirmed Transactions
                 </router-link>
                 <router-link :to="{name: 'pending'}" class="breadcrumb dark" v-else>
@@ -15,17 +15,40 @@
             </div>
         </div>
 
-        <div v-if="transaction" class="row tx-container">
-            <div class="col s12">
-                <div class="card">
-                    <div class="card-content">
-                        <span class="card-title">
-                            <b>Tx: </b>
-                            <i>{{ transaction.transactionDataHash }}</i>
-                        </span>
+        <div>
+            <div class="card">
+                <div class="card-content">
 
-                        <table class="highlight">
-                            <tbody>
+                    <!-- title -->
+                    <div class="card-title mb-4">
+                        <b>Tx: </b>
+                        <i>{{ $route.params.txhash }}</i>
+                    </div>
+
+                    <!-- loading -->
+                    <div v-if="isLoading" class="progress">
+                        <div class="indeterminate"></div>
+                    </div>
+
+                    <!-- loaded -->
+                    <div v-else>
+
+                        <!-- error -->
+                        <div v-if="errorMessage" class="red card white-text p-2">
+                            <p class="m-0">
+                                <span>{{ errorMessage }}</span>
+                                <button type="button"
+                                        @click.prevent="loadTransaction"
+                                        class="btn btn-small ml-3">
+                                    Reload
+                                </button>
+                            </p>
+                        </div>
+
+                        <!-- no error -->
+                        <div v-else>
+                            <table v-if="transaction" class="highlight">
+                                <tbody>
                                 <tr>
                                     <td><b>From:</b></td>
                                     <td>
@@ -89,9 +112,9 @@
                                     </td>
                                 </tr>
 
-                            </tbody>
-                        </table>
-
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -99,36 +122,33 @@
     </div>
 </template>
 
-<style scoped>
-    table {
-        margin-top: 5vh;
-    }
-</style>
-
 <script>
     const axios = require('axios');
     const timeago = require('timeago.js');
+    import utils from '../../../library/utils';
 
     export default {
         name: "TxHash",
         data() {
             return {
-                transaction: undefined
+                transaction: undefined,
+                isLoading: false,
+                errorMessage: ''
             }
         },
         methods: {
             async loadTransaction() {
+                this.isLoading = true;
                 try {
                     const url = window.APP_CONFIG.blockchain_node_url;
                     const response = await axios.get(`${url}/transactions/${this.$route.params.txhash}`);
-                    console.log(response.data);
-
                     this.transaction = response.data;
                     this.transaction.age = timeago.format(response.data.dateCreated);
                 }
                 catch(error) {
-                    console.log(error);
+                    this.errorMessage = utils.getErrorMessage(error);
                 }
+                this.isLoading = false;
             }
         },
         mounted() {
